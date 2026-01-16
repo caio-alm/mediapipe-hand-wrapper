3359fff98476f3367e9d57b4f8c36f3c565c0058
caio-alm <almendracaio@gmail.com>
2026-01-16 17:17:00 -0300

    Added hand sign recognition

--- handDetected.py
+++ handDetected.py
@@ -4,6 +4,11 @@ import mediapipe as mp
 
 webcam_image = np.ndarray
 rgb_tuple = tuple[int, int, int]
+signals = {
+    "Peace": [False, True, True, False, False],
+    "Hang Lose": [True, False, False, False, True],
+    "Rock": [False, True, False, False, True]
+}
 
 
 class Detector:
@@ -30,7 +35,7 @@ class Detector:
         self.mp_draw = mp.solutions.drawing_utils
 
 
-    def find_hands(self, img: webcam_image, draw_hands: bool = True):
+    def findHands(self, img: webcam_image, draw_hands: bool = True):
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         self.results = self.hands.process(img_rgb)
@@ -41,6 +46,26 @@ class Detector:
         return img
 
 
+    def raisedFingers(self, hand):
+        fingers = []
+        fingersHand = [4, 8, 12, 16, 20]
+
+        if hand.landmark[fingersHand[0]].x > hand.landmark[fingersHand[0]-2].x:
+            fingers.append(True)
+
+        else:
+            fingers.append(False)
+
+        for f in range(1, 5):
+            if hand.landmark[fingersHand[f]].y < hand.landmark[fingersHand[f] - 2].y:
+                fingers.append(True)
+
+            else:
+                fingers.append(False)
+
+        return fingers
+
+
 if __name__ == '__main__':
     Detec = Detector()
 
@@ -48,7 +73,21 @@ if __name__ == '__main__':
     while True:
         _, image = capture.read()
 
-        image = Detec.find_hands(image)
+        image = Detec.findHands(image)
+
+        if Detec.results and Detec.results.multi_hand_landmarks:
+            hand_rigth_obj = Detec.results.multi_hand_landmarks[0]
+
+            raisedFingers = Detec.raisedFingers(hand_rigth_obj)
+
+            print(raisedFingers)
+
+            total = raisedFingers.count(1)
+            cv2.putText(image, f'Rigth Fingers: {total}', (30, 30), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 1)
+
+            for s, v in signals.items():
+                if raisedFingers == v:
+                    cv2.putText(image, f'Signal: {s}', (30, 60), cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 0), 1)
 
         cv2.imshow('Camera', image)
 
